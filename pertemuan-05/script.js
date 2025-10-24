726f05b2a2a6450b1ade3acc466b75a152820d9a
Deskia Adminitasari <2511500003@mahasiswa.atmaluhur.ac.id>
2025-10-24 16:05:19 +0700

    memperbaiki typo

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,3 @@
-dokument.getElementById("menuToggle").addEventListener("click", function () {
+document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
 });
