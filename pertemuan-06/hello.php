1c542ad4e336931a37ed70b768df4e09f36853a8
Deskia Adminitasari <2511500003@mahasiswa.atmaluhur.ac.id>
2025-11-04 12:57:29 +0700

    latihan operator percabangan

--- pertemuan-06/hello.php
+++ pertemuan-06/hello.php
@@ -87,3 +87,14 @@ echo "\$a === \$b : "; var_dump($a === $b);
 echo "\$c == \$d : "; var_dump($c == $d);
 echo "\$c === \$d : "; var_dump($c === $d);
 ?>
+
+<?php
+$nilai = 80;
+if ($nilai >= 90) {
+echo "A";
+} elseif ($nilai >= 80) {
+echo "B";
+} else {
+echo "C";
+}
+?>
